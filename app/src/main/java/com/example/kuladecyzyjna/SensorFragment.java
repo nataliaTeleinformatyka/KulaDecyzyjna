b8fbf329a564409d96cff506d33ab242b9f1bb12
natalia.teleinformatyka <natalia868@op.pl>
2019-05-29 17:31:23 +0200

    Version 2

---
+++ app/src/main/java/com/example/kuladecyzyjna/SensorFragment.java
@@ -0,0 +1,19 @@
+package com.example.kuladecyzyjna;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SensorFragment extends ListFragment {
+
+    public SensorFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
